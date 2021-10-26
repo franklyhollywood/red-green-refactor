69e1e2b495926f1a55e1b8fff82505b2b66adad7
Frank Hollywood <github@srvsync.com>
2021-10-26 10:56:01 -0700

    started test for caps

--- CopyPush.js
+++ CopyPush.js
@@ -5,7 +5,3 @@ function copyPush(array, item) {
 }
 
 module.exports = copyPush;
-
-//oops we were supposed to return a copy
-//add another test that makes sure that the original array is unchanged
-//update code, use spread to copy and append
--- capatilizeFilter.test.js
+++ capatilizeFilter.test.js
@@ -1,7 +1,9 @@
 const capatilizeFilter = require('./CapatilizeFilter');
 
 describe('capatilizeFilter', () => {
-    it('capitalizes all strings in array and filter out any string that start with the letter F/f', () => )
+    it('capitalizes all strings in array and filter out any string that start with the letter F/f', () => {
+        const noCap = ['a', 'b', 'c']
+    }
 
-    
+
 })
