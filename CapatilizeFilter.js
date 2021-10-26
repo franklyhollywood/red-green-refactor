31fad365d7504c603e983c8ac97bcc4bdef9489e
Frank Hollywood <github@srvsync.com>
2021-10-26 11:14:05 -0700

    Failing cap. test

---
+++ CapatilizeFilter.js
@@ -0,0 +1,3 @@
+function capitalizeFilter() {}
+
+module.exports = capitalizeFilter;
--- capatilizeFilter.test.js
+++ capatilizeFilter.test.js
@@ -1,8 +1,11 @@
-const capatilizeFilter = require('./CapatilizeFilter');
+const capatilizeFilter = require('./CapitalizeFilter.js');
 
 describe('capatilizeFilter', () => {
     it('capitalizes all strings in array and filter out any string that start with the letter F/f', () => {
-        const noCap = ['a', 'b', 'c']
+        const noCap = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+        expect(capitalizeFilter(noCap)).toEqual(['A', 'B', 'C', 'D', 'E', 'G'] 
+    )
     }
 
 
