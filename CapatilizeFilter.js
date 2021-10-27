e8e7ab1d734160b9c5e239035cb017fa2bec5fbb
Frank Hollywood <github@srvsync.com>
2021-10-27 08:49:51 -0700

    failing test in Capitalize and Filter

--- CapatilizeFilter.js
+++ CapatilizeFilter.js
@@ -1,3 +1,12 @@
-function capitalizeFilter() {}
+function capitalizeFilter(array) {}
+
+const newArray = [];
+
+for (const item of array) {
+  const upperCase = item.toUpperCase();
+  newArr.push(upperCase);
+
+  return newArr.filter((words) => words.startsWith('F'));
+}
 
 module.exports = capitalizeFilter;
--- capatilizeFilter.test.js
+++ capatilizeFilter.test.js
@@ -2,11 +2,9 @@ const capatilizeFilter = require('./CapitalizeFilter.js');
 
 describe('capatilizeFilter', () => {
     it('capitalizes all strings in array and filter out any string that start with the letter F/f', () => {
-        const noCap = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+        const noCap = ['animal', 'ball', 'cookie', 'dog', 'elephant', 'fear', 'gold']
 
-        expect(capitalizeFilter(noCap)).toEqual(['A', 'B', 'C', 'D', 'E', 'G'] 
-    )
+        expect(capitalizeFilter(noCap)).toEqual(['ANIMAL', 'BALL', 'COOKIE', 'DOG', 'ELEPHANT', 'GOLD'])
     }
-
-
-})
+});
+``
