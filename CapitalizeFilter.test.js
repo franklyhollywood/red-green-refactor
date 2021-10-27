1ff2afcafaa78e01d37ef943a6b9531999fd35d7
Frank Hollywood <github@srvsync.com>
2021-10-27 09:16:38 -0700

    fixed test

---
+++ CapitalizeFilter.test.js
@@ -0,0 +1,24 @@
+const capitalizeFilter = require('./CapitalizeFilter.js');
+
+describe('capatilizeFilter', () => {
+  it('capitalizes all strings in array and filter out any string that start with the letter F/f', () => {
+    const noCap = [
+      'animal',
+      'ball',
+      'cookie',
+      'dog',
+      'elephant',
+      'fear',
+      'gold',
+    ];
+
+    expect(capitalizeFilter(noCap)).toEqual([
+      'ANIMAL',
+      'BALL',
+      'COOKIE',
+      'DOG',
+      'ELEPHANT',
+      'GOLD',
+    ]);
+  });
+});
